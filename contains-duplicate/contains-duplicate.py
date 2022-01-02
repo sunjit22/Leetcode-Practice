719da675c7e60abc26edc8b5d09190fd7204f2a7
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-01-02 16:17:36 -0500

    Time: 551 ms (11.96%), Space: 23.9 MB (26.22%) - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -5,10 +5,17 @@ class Solution(object):
         :rtype: bool
         """
         
-        set_nums = set(nums)
-        return len(set_nums) < len(nums)
+        # APPROACH 1
+        return len(set(nums)) != len(nums)
+        
+        # OTHER APPROACHES
+        
+        # APPROACH 2
+        # set_nums = set(nums)
+        # return len(set_nums) < len(nums)
         
          
+        # APPROACH 3    
         # set_nums = set()
         # for i in range(len(nums)):
         #     if nums[i] in set_nums:
@@ -17,7 +24,8 @@ class Solution(object):
         #         set_nums.add(nums[i])
         # return False        
 
-                
+        
+        # APPROACH 4
         # nums.sort();
         # for i in range(0, len(nums)-1):
         #     if nums[i]==nums[i+1]:
