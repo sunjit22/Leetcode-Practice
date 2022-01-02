3af7877fdd66d281bd80db8e4d597f392c5686c2
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-01-02 16:15:42 -0500

    Time: 541 ms (12.22%), Space: 24 MB (17.83%) - LeetHub

--- contains-duplicate/contains-duplicate.py
+++ contains-duplicate/contains-duplicate.py
@@ -4,8 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        nums.sort();
-        for i in range(0, len(nums)-1):
-            if nums[i]==nums[i+1]:
-                return True  
-        return False      
+        
+        set_nums = set(nums)
+        return len(set_nums) < len(nums)
+        
+         
+        # set_nums = set()
+        # for i in range(len(nums)):
+        #     if nums[i] in set_nums:
+        #         return True
+        #     else:
+        #         set_nums.add(nums[i])
+        # return False        
+
+                
+        # nums.sort();
+        # for i in range(0, len(nums)-1):
+        #     if nums[i]==nums[i+1]:
+        #         return True  
+        # return False      
