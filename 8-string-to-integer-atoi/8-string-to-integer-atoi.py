96bb5191879d154be8f55555c79112f0131aa924
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-01-04 03:31:27 -0500

    Time: 32 ms (83.90%), Space: 14.2 MB (81.91%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -0,0 +1,30 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        result = ""
+        array = list(s)
+        sign = +1
+        if(len(array) == 0):
+            return 0
+        if(array[0] == "-"):
+            sign = -1 
+        
+        if (array[0] == "-" or array[0] == "+"):
+            del array[0]
+                
+        
+        i = 0 
+        result = 0
+        while(i < len(array) and array[i].isdigit()):
+            result = result * 10 + ord(array[i]) - ord('0')
+            i += 1
+              
+        result = result * sign  
+        
+
+        if(result < pow(-2,31)):
+            result = pow(-2,31) 
+        if(result >= pow(2,31)):
+            result = pow(2,31) - 1
+        return result
+        
