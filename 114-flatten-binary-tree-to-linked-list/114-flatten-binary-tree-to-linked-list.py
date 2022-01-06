d9214970b90765cb5489dad8c071dcdbd490e9a0
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-01-06 12:56:17 -0500

    Time: 85 ms (5.18%), Space: 15.3 MB (14.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -5,28 +5,52 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, root):
+#   Approach 1   
+    def flatten(self, root: Optional[TreeNode]) -> None:
         if not root:
             return None
 
-        if not root.left and not root.right:
-            return root
-        
-        leftTail = self.dfs(root.left)
-        rightTail = self.dfs(root.right)
+        stack = []
+        stack.append(root)
 
-        if leftTail:
-            leftTail.right = root.right
-            root.right = root.left
-            root.left = None
+        prev = None
 
-        return rightTail if rightTail else leftTail
+        while stack:
+            s = stack.pop()
+            if s:
+                stack.append(s.right)
+                stack.append(s.left)
+
+                if prev:
+                    prev.right = s
+                    prev.left = None
+                    s.left = None
+
+                prev = s
+            
+#     Approach 2 - Recursion O(n)          
+#     def dfs(self, root):
+#         if not root:
+#             return None
+
+#         if not root.left and not root.right:
+#             return root
         
-    def flatten(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """        
-        self.dfs(root)       
+#         leftTail = self.dfs(root.left)
+#         rightTail = self.dfs(root.right)
+
+#         if leftTail:
+#             leftTail.right = root.right
+#             root.right = root.left
+#             root.left = None
+
+#         return rightTail if rightTail else leftTail
+        
+#     def flatten(self, root: Optional[TreeNode]) -> None:
+#         """
+#         Do not return anything, modify root in-place instead.
+#         """        
+#         self.dfs(root)       
                 
                     
             
