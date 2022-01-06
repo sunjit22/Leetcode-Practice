d3160cfd70cf027b0dd25bf9c9f0509b622b6fd8
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-01-06 14:29:35 -0500

    Time: 103 ms (6.83%), Space: 18 MB (76.08%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -8,7 +8,7 @@ class Solution:
     def height(self, root):
         if root is None:
             return 0
-        
+
         return max(self.height(root.left), self.height(root.right)) + 1
         
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
