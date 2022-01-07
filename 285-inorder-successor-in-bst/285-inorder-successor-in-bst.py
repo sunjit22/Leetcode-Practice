c6e3561f34d15f6746d0271cf07b31d72b54fe9d
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-01-06 20:14:56 -0500

    Time: 121 ms (10.27%), Space: 18.2 MB (45.57%) - LeetHub

---
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderSuccessor(self, root: 'TreeNode', p: 'TreeNode') -> 'Optional[TreeNode]':
+        if p.right:
+            curr = p.right
+            while curr.left:
+                curr = curr.left
+            return curr
+        else:
+            successor, curr = None, root
+            while curr != p:
+                if curr.val < p.val:
+                    curr = curr.right
+                else:
+                    successor, curr = curr, curr.left
+            return successor
