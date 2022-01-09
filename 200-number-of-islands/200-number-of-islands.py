d0ba1b6f49248f1c17f35bb68b07caef5b5fc725
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-01-08 21:13:00 -0500

    Time: 314 ms (68.49%), Space: 16.8 MB (53.89%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,26 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # Check whether grid exists
         if grid == None or len(grid) == 0:
             return 0
+        
+        # Prereq:
         rows = len(grid)
         cols = len(grid[0])
         visited = []
         
+        # Create a matrix to store track of visits
         for row in range(rows):
             visited.append([False]*cols)
-        
-        
-        
-        def traverse_grid(i,j):
+ 
+        # Dfs 
+        def dfs(i,j):
             if i < 0 or j < 0 or i > (rows-1) or j > (cols-1) or grid[i][j] == '0' or visited[i][j]:
                 return 
-            
             grid[i][j] == "0"
             visited[i][j] = True
-            traverse_grid(i-1, j)
-            traverse_grid(i+1, j)
-            traverse_grid(i, j-1)
-            traverse_grid(i, j+1)
+            dfs(i-1, j)
+            dfs(i+1, j)
+            dfs(i, j-1)
+            dfs(i, j+1)
             
             
         num_islands = 0
@@ -28,7 +30,7 @@ class Solution:
             for j in range(cols):
                 if grid[i][j] == "1" and not visited[i][j]:
                     num_islands += 1
-                    traverse_grid(i,j)
+                    dfs(i,j)
                         
         return num_islands   
     
