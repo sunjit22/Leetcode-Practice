92f2552bca8e6921a438d78cc53797cb685776a4
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-01-10 00:00:33 -0500

    Time: 71 ms (58.97%), Space: 14.4 MB (48.37%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+  
+                
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        # Create a hashmap
+        hashmap = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i;
+                
+                
+ 
