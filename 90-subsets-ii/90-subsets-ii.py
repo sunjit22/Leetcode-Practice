42d5875ef85ad8b6ad06faadd3e63fd1036bae93
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-01-14 22:39:06 -0500

    Time: 32 ms (93.95%), Space: 14.7 MB (7.07%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        output = [[]]
+        
+        for num in nums:
+            # for curr in output:
+            #     output.append([curr + [num]])
+            output += [curr + [num] for curr in output]
+        
+        result = []
+        for o in output:
+            if o not in result:
+                result.append(o)
+        return sorted(result)      
