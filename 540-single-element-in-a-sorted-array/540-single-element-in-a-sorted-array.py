7d611bd80b2ab07607b1be25fa2fea891dcab26d
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-01-16 00:22:20 -0500

    Time: 176 ms (17.78%), Space: 24.3 MB (7.40%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,8 +1,34 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        sum_ = sum(nums)
-        set_nums = set(nums)
+#         Approach 1 : O(n)        
+#         sum_ = sum(nums)
+#         set_nums = set(nums)
+#         sum_set = sum(set_nums) * 2
+#         return sum_set - sum_
+
+#         Approach 2 : O(log n)
+
+        low = 0
+        high = len(nums) - 1
         
-        sum_set = sum(set_nums) * 2
-        
-        return sum_set - sum_
+        while low < high:
+            mid = (low+high)//2
+            
+            if nums[mid] != nums[mid+1] and nums[mid] != nums[mid-1]:
+                return nums[mid]
+            
+            if nums[mid] == nums[mid+1] :
+                num_left = mid-low
+                if num_left % 2 == 0:
+                    low = mid+2
+                else:
+                    high = mid-1
+            elif nums[mid] == nums[mid-1]:
+                num_left = (mid-1)-low
+                if num_left % 2 == 0:
+                    low = mid+1
+                else:
+                    high = mid-2
+                    
+        return nums[low]              
+                
