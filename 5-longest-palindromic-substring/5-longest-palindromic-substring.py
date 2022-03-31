a55f106a56f64db7592973dfdc285774c6952baf
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-03-30 21:08:13 -0400

    Time: 848 ms (87.80%), Space: 13.9 MB (63.72%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,41 +1,41 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:     
-        if s == "" or len(s) == 0:
-            return ""
+#         if s == "" or len(s) == 0:
+#             return ""
         
-        n=len(s)
-        res=0
+#         n=len(s)
+#         res=0
         
-        def spread(i,j):
-            while i>=0 and j<n and s[i]==s[j]:
-                i-=1
-                j+=1
-            return s[i+1:j]
+#         def spread(i,j):
+#             while i>=0 and j<n and s[i]==s[j]:
+#                 i-=1
+#                 j+=1
+#             return s[i+1:j]
         
-        res=""
-        for i in range(n):
-            res=max(spread(i,i), spread(i,i+1),res, key=len)
+#         res=""
+#         for i in range(n):
+#             res=max(spread(i,i), spread(i,i+1), res, key=len)
         
-        return res
+#         return res
         
-#         start = 0
-#         end = 0
+        start = 0
+        end = 0
         
-#         for i in range(len(s)):
-#             len1 = self.expand_around_center(s, i, i)
-#             len2 = self.expand_around_center(s, i, i+1)
+        for i in range(len(s)):
+            len1 = self.expand_around_center(s, i, i)
+            len2 = self.expand_around_center(s, i, i+1)
 
-#             length = max(len1, len2)
-#             if length > (end-start):
-#                 start = i - (length-1)/2
-#                 end = i + length/2
-#         return s[int(start):int(end+1)]
+            length = max(len1, len2)
+            if length > (end-start):
+                start = i - (length-1)//2
+                end = i + length//2
+        return s[int(start):int(end+1)]
             
-#     def expand_around_center(self, s, left, right):
-#         while (left >= 0 and right < len(s) and s[left] == s[right]):
-#             left -= 1
-#             right += 1
-#         return right - left - 1          
+    def expand_around_center(self, s, left, right):
+        while (left >= 0 and right < len(s) and s[left] == s[right]):
+            left -= 1
+            right += 1
+        return right - left - 1          
         
 #         Approach 2 : Exceeds time limit        
 #         n = len(st) # get length of input string
