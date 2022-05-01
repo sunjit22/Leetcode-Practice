9ad6cf07176d1dcd9af066f52cd9ec9ae9418288
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-05-01 15:33:10 -0400

    Time: 27 ms (94.89%), Space: 13.8 MB (97.58%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.py
+++ 461-hamming-distance/461-hamming-distance.py
@@ -1,27 +1,37 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        x = format(x, "b")
-        y = format(y, "b")
+        # Appraoch 1
+        # Take XOR of x and y : xor returns 1 if x and y are different
+        # Then count number of ones in bit representation of result of xor
         
-        length_x = len(x)
-        length_y = len(y)
+        return format(x^y,"b").count("1")
         
-        length = max(length_x, length_y)
         
-        if length_x < length:
-            difference = length - length_x
-            st = "0"*difference
-            x = st + x
-        elif length_y < length:
-            difference = length - length_y
-            st = "0"*difference
-            y = st + y
         
-        count = 0
-        for i in range(len(x)):
-            if x[i] != y[i]:
-                count += 1
+        
+#         APPROACH 2        
+#         x = format(x, "b")
+#         y = format(y, "b")
+        
+#         length_x = len(x)
+#         length_y = len(y)
+        
+#         length = max(length_x, length_y)
+        
+#         if length_x < length:
+#             difference = length - length_x
+#             st = "0"*difference
+#             x = st + x
+#         elif length_y < length:
+#             difference = length - length_y
+#             st = "0"*difference
+#             y = st + y
+        
+#         count = 0
+#         for i in range(len(x)):
+#             if x[i] != y[i]:
+#                 count += 1
                 
-        return count        
+#         return count        
             
             
