f7279b1e5e63a4e2432b596b31e54b9931bf22d9
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-05-03 01:05:02 -0400

    Time: 65 ms (58.02%), Space: 13.9 MB (69.40%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -7,6 +7,8 @@ class Solution:
             else:
                 hashmap[nums[i]] = 1
         
+        # Sort first by freq in hashmap
+        # If same frew -> then sort in decreasing order
         nums.sort(key=lambda x: (hashmap[x], -x))
         return nums   
         
