bb83cbb3bce73999d5674d6474c41e6af11d87f3
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-05-03 01:10:00 -0400

    Time: 252 ms (32.24%), Space: 15.5 MB (36.83%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,48 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+#         dict = {}
+        
+#         for i in range(len(nums)):
+#             dict[nums[i]] = dict.get(nums[i],0) + 1
+#         print(dict)    
+#         arr = []    
+#         for key,value in dict.items():
+#             arr.append([value,key])
+         
+#         maximum = max(arr)
+#         return maximum[1]  
+        # min=len(nums)/2
+        # s=set(nums)
+        # for num in s:
+        #     if(nums.count(num)>min):
+        #         return num
+        
+        # nums.sort()
+        # return nums[len(nums)//2]
+        
+        hashmap = {}        
+        for i in range(len(nums)):
+            if nums[i] in hashmap:
+                hashmap[nums[i]] += 1
+            else:
+                hashmap[nums[i]] = 1
+        
+        nums.sort(key = lambda x:hashmap[x])
+        
+        return nums[-1]
+        
+        
+        
+#         count = 0
+#         candidate = None
+
+#         for num in nums:
+#             if count == 0:
+#                 candidate = num
+                
+#             if num == candidate:
+#                 count += 1
+#             else:
+#                 count -= 1
+
+#         return candidate
