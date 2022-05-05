7d567b864c64141b860563e0569527677f792f67
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-05-04 22:36:19 -0400

    Time: 46 ms (39.40%), Space: 13.9 MB (19.04%) - LeetHub

--- 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
+++ 1592-rearrange-spaces-between-words/1592-rearrange-spaces-between-words.py
@@ -1,34 +1,49 @@
 class Solution:
     def reorderSpaces(self, text: str) -> str:
-        words = text.split()
-        num_words=len(words)
+        words_list = text.split()
+        num_spaces = text.count(' ')
+        num_words = len(words_list)
         
-        count = 0
-        for i in range(len(text)):
-            if text[i] == " ":
-                count += 1
-                
         if num_words == 1:
-            result = words[0] 
-            for i in range(count):
-                result += " "
-            return result    
-                
-        formula = int(count/(num_words-1))
+            return(words_list[0] + ' ' * num_spaces)
+        else:
+            q, r = divmod(num_spaces, num_words - 1)
+            return( (' ' * q).join(words_list) + ' ' * r )
+
+    
+    
+    
+    
+        # Approach 2
+#         words = text.split()
+#         num_words=len(words)
         
-        result = ""
-        for i in range(num_words):
-            result += words[i]
-            if i != num_words-1:
-                for j in range(formula):
-                    result += " " 
-                count -= formula
+#         count = 0
+#         for i in range(len(text)):
+#             if text[i] == " ":
+#                 count += 1
+                
+#         if num_words == 1:
+#             result = words[0] 
+#             for i in range(count):
+#                 result += " "
+#             return result    
+                
+#         formula = int(count/(num_words-1))
+        
+#         result = ""
+#         for i in range(num_words):
+#             result += words[i]
+#             if i != num_words-1:
+#                 for j in range(formula):
+#                     result += " " 
+#                 count -= formula
                 
             
-        if count > 0:
-            for i in range(count):
-                result += " "
-        return result    
+#         if count > 0:
+#             for i in range(count):
+#                 result += " "
+#         return result    
         
         
         
