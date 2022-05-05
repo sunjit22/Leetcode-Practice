61c7193efaab60d30dfe24cfdd367ad18289a28e
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-05-04 22:17:32 -0400

    Time: 46 ms (57.38%), Space: 14 MB (34.03%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,25 +1,34 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-
-        s = list(s)
-        t = list(t)
+        s = sorted(s, key = lambda x: ord(x))
+        t = sorted(t, key = lambda x: ord(x))
         
-        len1 = len(s)
-        len2 = len(t)
-        if len1 == len2:
-            return ""
-        elif len1 > len2:
-            for i in range(len(s)):
-                if s[i] in t:
-                    t[t.index(s[i])] = "0"
-                else:    
-                    return s[i]
-        else:
-            for i in range(len(t)):
-                if t[i] in s:
-                    s[s.index(t[i])] = "0"
-                else:
-                    return t[i]
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return t[i]
+        return t[-1]    
+        
+        
+        
+#         s = list(s)
+#         t = list(t)
+        
+#         len1 = len(s)
+#         len2 = len(t)
+#         if len1 == len2:
+#             return ""
+#         elif len1 > len2:
+#             for i in range(len(s)):
+#                 if s[i] in t:
+#                     t[t.index(s[i])] = "0"
+#                 else:    
+#                     return s[i]
+#         else:
+#             for i in range(len(t)):
+#                 if t[i] in s:
+#                     s[s.index(t[i])] = "0"
+#                 else:
+#                     return t[i]
             
        
         
