567f17f822d429ae23b0836082fa3370a3717493
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-06-01 16:14:55 -0400

    Time: 737 ms (51.21%), Space: 36.2 MB (34.90%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,18 +1,34 @@
 class MedianFinder:
-
     def __init__(self):
-        self.nums = []
+        self.heaps = [], []
 
-    def addNum(self, num: int) -> None:
-        self.nums.append(num)
+    def addNum(self, num):
+        small, large = self.heaps
+        heappush(small, -heappushpop(large, num))
+        if len(large) < len(small):
+            heappush(large, -heappop(small))
 
-    def findMedian(self) -> float:
-        n = len(self.nums)
-        self.nums.sort()
-        if n % 2 != 0 :
-            return self.nums[n//2]
-        else:
-            return (self.nums[n//2] + self.nums[(n//2)-1])/2 
+    def findMedian(self):
+        small, large = self.heaps
+        if len(large) > len(small):
+            return float(large[0])
+        return (large[0] - small[0]) / 2.0
+    
+    
+    
+#     def __init__(self):
+#         self.nums = []
+
+#     def addNum(self, num: int) -> None:
+#         self.nums.append(num)
+
+#     def findMedian(self) -> float:
+#         n = len(self.nums)
+#         self.nums.sort()
+#         if n % 2 != 0 :
+#             return self.nums[n//2]
+#         else:
+#             return (self.nums[n//2] + self.nums[(n//2)-1])/2 
              
 
 
