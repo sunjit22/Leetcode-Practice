4e405cbe24a002b7ec52d9f3882b2a19c895301f
Sunjit Dhillon <63877211+sunjit22@users.noreply.github.com>
2022-06-19 14:10:58 -0400

    Time: 5 ms (41.94%), Space: 49.4 MB (71.43%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,37 +1,43 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // write your code here
-        int len = nums1.length + nums2.length;
-        if (len % 2 == 1) {
-            return finKth(nums1, 0, nums2, 0, len / 2 + 1);
-        } else {
-            return (
-                finKth(nums1, 0, nums2, 0, len / 2) + finKth(nums1, 0, nums2, 0, len / 2 + 1)
-                ) / 2.0;
-        }
-    }
-    
-    private double finKth(int[] nums1, int indA, int[] nums2, int indB, int k) {
-        if (indA >= nums1.length) {
-            return nums2[indB + k - 1];
-        }
-        if (indB >= nums2.length) {
-            return nums1[indA + k - 1];
-        }
-        if (k == 1) {
-            return Math.min(nums1[indA], nums2[indB]);
+        
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] mergedarray = new int[m+n];
+        
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        
+        while(i < nums1.length && j < nums2.length) {
+            if(nums1[i] < nums2[j]) {
+                mergedarray[k++] = nums1[i++];
+            } else {
+                mergedarray[k++] = nums2[j++];
+            }
         }
         
-        int A_key = indA + k / 2 - 1 < nums1.length 
-                    ? nums1[indA + k / 2 - 1]
-                    : Integer.MAX_VALUE;
-        int B_key = indB + k / 2 - 1 < nums2.length
-                    ? nums2[indB + k / 2 - 1]
-                    : Integer.MAX_VALUE;
-        if (A_key > B_key) {
-            return finKth(nums1, indA, nums2, indB + k / 2, k - k / 2);
-        } else {
-            return finKth(nums1, indA + k / 2, nums2, indB, k - k / 2);
+        while(i < nums1.length) {
+            mergedarray[k++] = nums1[i++];
         }
+        
+        while(j < nums2.length) {
+            mergedarray[k++] = nums2[j++];
+        }
+        
+        int l = mergedarray.length;
+        
+        double avg;
+        int mid = l/2;
+       
+        
+        if (l%2 == 0) {
+            double sum = mergedarray[mid-1] + mergedarray[mid];
+            avg = (sum/2); 
+        } else {
+            avg = (Math.floor(mergedarray[mid]));
+        }
+        
+        return avg;
     }
 }
